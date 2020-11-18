758f30e26e45c6920d7db2d00aa1a01c8cb747da
edimarf <edmarfyelloww@gmail.com>
2020-11-18 21:04:20 +0100

    AddPost --> ImagePicker working fine

--- pipocar/src/components/screenForms/AddPostForm.js
+++ pipocar/src/components/screenForms/AddPostForm.js
@@ -11,9 +11,95 @@ import {
 InputForPosts,
 Button
 } from '../reusebleComponents/index';
+import ImagePicker from 'react-native-image-crop-picker';
+import {check, PERMISSIONS, RESULTS} from 'react-native-permissions';
 class  AddPostForm extends Component {
+   //Image Picker 
+  _checkPermissions = () => {
+    check(PERMISSIONS.IOS.LOCATION_ALWAYS)
+    .then((result) => {
+    switch (result) {
+    case RESULTS.UNAVAILABLE:
+    console.log(
+    'This feature is not available (on this device / in this context)',
+    );
+    break;
+    case RESULTS.DENIED:
+    console.log(
+    'The permission has not been requested / is denied but requestable',
+    );
+    break;
+    case RESULTS.GRANTED:
+    console.log('The permission is granted');
+    break;
+    case RESULTS.BLOCKED:
+    console.log('The permission is denied and not requestable anymore');
+    break;
+    }
+    })
+    .catch((error) => {
+    // …
+    console.log('Unknown Error', error);
+    });
+    } 
+          
+              //S4
+    s4 = () => {
+                  return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+                };
+          
+              //UploadPublish
+    UploadPublish = () => {
+         
+                    this.uploadImage(this.state.uri);
+          
+     
 
+  
+               
+              }
+              
+              //Unique ID
+              uniqueId = () => {
+                  return (
+                    this.s4() + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4()
+                  );
+                };
+          
+          
+              //ImagePicker
+              findNewImage = async () => {
+                  this._checkPermissions();
+          
+          
+                  //Here we open the camera
+                  let result = await ImagePicker.openPicker({
+                      mediaTypes: 'Images',
+                      width: 300,
+                      height: 400,
+                      cropping: true,
+  
 
+                  });
+                  console.log(result);
+                  if(!result.cancelled)
+                  {
+                      console.log('upload image');
+                      this.setState({
+                          imageSelected: true,
+                          imageId: this.uniqueId(),
+                          uri: result.uri
+                      })
+          
+                  }else{
+                      console.log('cancel');
+                      this.setState({
+                          imageSelected: false
+                        });
+                  } 
+              }
 
 
 render(){ 
@@ -21,7 +107,8 @@ render(){
   return (
     <View style={{ marginLeft:10, marginRight:10}}>
       <View>
-        <TouchableOpacity
+        <TouchableOpacity 
+        onPress={this.findNewImage}
         style={{
           width:100,
           height:100,
